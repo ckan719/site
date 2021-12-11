ed0427edfa69f8edb265cdea94e1203b244cbf6d
ckan719 <nhat_1751220075@dau.edu.vn>
2021-12-11 08:43:49 +0700

    Revert "[!!] fix bug"
    This reverts commit d818aa9615ef4c0006809b2f2a612538b4e5c3a9.

--- dmoj/local_settings.py
+++ dmoj/local_settings.py
@@ -17,7 +17,7 @@ DEBUG = False  # Change to False once you are done with runserver testing.
 
 # Uncomment and set to the domain names this site is intended to serve.
 # You must do this once you set DEBUG to False.
-ALLOWED_HOSTS = ['192.168.1.6']
+ALLOWED_HOSTS = ['192.168.1.39', 'dauj.edu.com']
 
 # Optional apps that DMOJ can make use of.
 INSTALLED_APPS += (
@@ -169,7 +169,7 @@ EVENT_DAEMON_USE = True
 #EVENT_DAEMON_POST = '<ws:// URL to post to>'
 
 # If you are using the defaults from the guide, it is this:
-EVENT_DAEMON_POST = 'ws://127.0.0.1:15101/'
+EVENT_DAEMON_POST = 'ws://192.168.1.39:15101/'
 
 # These are the publicly accessed interface configurations.
 # They should match those used by the script.
@@ -179,8 +179,8 @@ EVENT_DAEMON_POST = 'ws://127.0.0.1:15101/'
 # i.e. the path to /channels/ exposed by the daemon, through whatever proxy setup you have.
 
 # Using our standard nginx configuration, these should be.
-EVENT_DAEMON_GET = 'ws://127.0.0.1/event/'
-EVENT_DAEMON_GET_SSL = 'wss://127.0.0.1/event/' # Optional
+EVENT_DAEMON_GET = 'ws://192.168.1.39/event/'
+EVENT_DAEMON_GET_SSL = 'wss://192.168.1.39/event/' # Optional
 EVENT_DAEMON_POLL = '/channels/'
 
 # If you would like to use the AMQP-based event server from <https://github.com/DMOJ/event-server>,
